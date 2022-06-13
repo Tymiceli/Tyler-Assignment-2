201aa3b95ccff5685be7b990262d39d83d985676
Tyler Miceli <tymiceli24@gmail.com>
2022-06-13 17:02:40 -0400

    v3 changes

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,13 +1,55 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// Variables
+		int x;
+		int guess;
+		boolean answerGuess = false;
+
+		// Game start line
+		System.out.println("Guess a number between 1 and 100");
+		Random random = new Random();
+		Scanner sc = new Scanner(System.in);
+
+		// Logic and while loop
+		while (!answerGuess) {
+
+			x = random.nextInt(100) + 1;
+
+			for (int attempts = 0; attempts < 5; attempts++) {	
+				
+				guess = sc.nextInt();
+
+				if (guess < 1 || guess > 100) {
+					System.out.println("Your guess is not between 1 and 100, please try again.");
+					attempts--;
+				}
+				if (guess != x) {
+
+					if (guess > x && guess >= 1 && guess <= 100) {
+						System.out.println("Please pick a lower number");
+//						attempts++;
+					}
+					if (guess < x && guess >= 1 && guess <= 100) {
+						System.out.println("Please pick a higher number");
+//						attempts++;
+					}
+					if (attempts == 4) {
+						System.out.println("You lose! The number to guess was " + x);
+						break;
+					}
+				} else {
+					System.out.println("You win!");
+					System.out.println("The number to guess was " + x + ".");
+					answerGuess = true;
+				}
+			}
+		}
 	}
-
-	// Generate a random number
-	// Read user input
-	
 }
